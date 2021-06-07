8290f31107476308d8930b128515cebacf1253fa
sekercih <hkkskrc@gmail.com>
2021-06-07 23:45:15 +0300

    hook

--- ornek8_eventsHooks/src/App.js
+++ ornek8_eventsHooks/src/App.js
@@ -1,8 +1,17 @@
-import Events from "./Events"
+import ClassComponent from "./ClassComponent";
+
+import Events from "./Events";
+import HookOrnek from "./HookOrnek";
+import Hooks from "./Hooks";
 function App() {
   return (
     <div className="App">
-     <Events/></div>
+      <Events />
+      <ClassComponent sayi="10" />
+      <ClassComponent sayi="100" />
+      <Hooks/>
+      <HookOrnek/>
+    </div>
   );
 }
 export default App;
