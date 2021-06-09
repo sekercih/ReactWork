618ef6785fbdba0e9de0051fe37fbe127f924e95
sekercih <hkkskrc@gmail.com>
2021-06-09 23:58:41 +0300

    fetch-axios-txt

--- ornek10_useffect/src/App.js
+++ ornek10_useffect/src/App.js
@@ -1,10 +1,18 @@
 import UseEffect01 from "./UseEffect01";
-
-
+import UseEffectAxios from "./UseEffectAxios";
+import UseEffectFetch from "./UseEffectFetch";
+import UseEffectFetchError from "./UseEffectFetchError";
+import UseEffectFetchTxt from "./UseEffectFetchTxt";
 
 function App() {
-  return <div className="App">
-    <UseEffect01/>
-  </div>;
+  return (
+    <div className="App">
+      {/* <UseEffect01 />
+      <UseEffectFetch /> */}
+      {/* <UseEffectFetchError /> */}
+      {/* <UseEffectAxios/> */}
+      <UseEffectFetchTxt/>
+    </div>
+  );
 }
 export default App;
---
+++ ornek11_hookuseeffect/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+function App() {
+ 
+  return (
+    <main>
+      <section className="container">
+       
+      </section>
+    </main>
+  );
+}
+
+export default App;
